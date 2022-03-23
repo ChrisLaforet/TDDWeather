2f118aa2109e72ace78f86a57e7953f9a973bbab
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-03-22 21:21:38 -0400

    Adding support for API calls and credentials

---
+++ TDDWeather/ApiSupport.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace TDDWeather
+{
+    public class ApiSupport
+    {
+        private string PerformGET(string url, string parameters)
+        {
+            HttpClient client;
+            using (client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+ 
+                HttpResponseMessage response = client.GetAsync(parameters).Result;  // TODO - FIX - Blocking call! Program will wait here until a response is received or a timeout occurs.
+                if (response.IsSuccessStatusCode)
+                {
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ TDDWeather/XMLCredentialsLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace TDDWeather
+{
+    public class XMLCredentialsLoader
+    {
+        private const string FILENAME = @"C:\Code\OpenWeatherKeys\Credentials.xml";
+
+        public string GetApiKey()
+        {
+            var fileElements = XElement.Load(FILENAME);
+            var element = fileElements.FirstNode;
+            if (element != null && element.NodeType == System.Xml.XmlNodeType.Text)
+            {
+                return element.ToString();
+            }
+            throw new KeyNotFoundException();
+        }
+    }
+}
