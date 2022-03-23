d6cd41aad0d6a8ef8fe567ef2c58b4148078bdc1
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-03-23 12:46:20 -0400

    Initial implementation of first openweathermap response

---
+++ TDDWeather/openweathermap/OwmLocationResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace TDDWeather
+{
+	public class OwmLocationResponse : ILocation
+	{
+		public string GetCity()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public double GetLatitude()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public double GetLongitude()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
