71cf8fb60fa524f2e8fb1131312e4a3f7224c591
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-03-23 13:13:03 -0400

    Initial steps towards handling json as a dom

--- TDDWeather/openweathermap/OwmLocationResponse.cs
+++ TDDWeather/openweathermap/OwmLocationResponse.cs
@@ -1,9 +1,19 @@
-﻿namespace TDDWeather
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace TDDWeather
 {
 	public class OwmLocationResponse : ILocation
 	{
+		private JsonNode document;
+
+		public OwmLocationResponse(string jsonResponse) => document = JsonNode.Parse(jsonResponse)!;
+
 		public string GetCity()
 		{
+			// int hotHigh = forecastNode["TemperatureRanges"]!["Hot"]!["High"]!.GetValue<int>();
+			//  JsonArray datesAvailable = forecastNode!["DatesAvailable"]!.AsArray()!;
+			// Console.WriteLine($"DatesAvailable[0]={datesAvailable[0]}");
 			throw new System.NotImplementedException();
 		}
 
