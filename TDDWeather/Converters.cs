2bc81454f93c1addda9756f8ba87a938137d1671
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-03-22 21:35:56 -0400

    First test

---
+++ TDDWeather/Converters.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDWeather
+{
+    public class Converters
+    {
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return 9.0 / 5.0 * celsius + 32.0;
+        }
+    }
+}
--- TDDWeather/WeatherOutlookTests.cs
+++ TDDWeather/WeatherOutlookTests.cs
@@ -2,9 +2,15 @@
 
 namespace TDDWeather
 {
-  
     public class TDDWeatherTests
     {
-        
+        [Fact]
+        public void GivenTemperatureInCelsius_WhenConverted_ThenReturnsFahrenheit()
+        {
+            Assert.Equal(122, Converters.CelsiusToFahrenheit(50));
+            Assert.Equal(68, Converters.CelsiusToFahrenheit(20));
+            Assert.Equal(32, Converters.CelsiusToFahrenheit(0));
+        }
+
     }
 }
