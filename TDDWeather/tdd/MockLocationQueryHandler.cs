2df7b6d4d47207dfb8b188473da342987b5bedea
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-03-23 15:39:49 -0400

    Preparing to implement first location tests

---
+++ TDDWeather/tdd/MockLocationQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDWeather
+{
+	public class MockLocationQueryHandler : ILocationQueryHandler
+	{
+		public ILocation GetLocationFor(string city, string country, string state)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ TDDWeather/tdd/TestHandlersProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace TDDWeather
+{
+	public class TestHandlersProvider : IHandlersProvider
+	{
+		public ILocationQueryHandler GetLocationQueryHandler()
+		{
+			return ILocationQueryHandler()
+			throw new System.NotImplementedException();
+		}
+	}
+}
