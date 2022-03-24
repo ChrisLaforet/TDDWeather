8082e51d74f712352365e4a221069ca97188d09c
Chris Laforet <laforet@chrislaforetsoftware.com>
2022-03-23 20:13:00 -0400

    Add logic to console app to show conditions

--- WeatherConsole/WeatherConsole.cs
+++ WeatherConsole/WeatherConsole.cs
@@ -3,6 +3,9 @@ using TDDWeather;
 
 class WeatherConsole
 {
+    private const double LATITUDE = 36.0957;
+    private const double LONGITUDE = -79.4378;
+    
     private static IHandlersProvider handlersProvider;
 
     public static void Main(string [] args)
@@ -18,6 +21,7 @@ class WeatherConsole
             Console.WriteLine("Weather Lookup");
             Console.WriteLine("--------------");
             Console.WriteLine();
+            Console.WriteLine("C. Current conditions for Burlington");
             Console.WriteLine("L. Location for Burlington");
             Console.WriteLine("X. Exit");
             Console.Write("Choice? ");
@@ -28,6 +32,10 @@ class WeatherConsole
             if (selection == 'L' || selection == 'l')
             {
                 HandleLocation();
+            } 
+            else if (selection == 'C' || selection == 'c')
+            {
+                HandleCurrentConditions();
             }
 
         } while (selection != 'X' && selection != 'x');
@@ -36,7 +44,7 @@ class WeatherConsole
     private static void HandleLocation()
     {
         Console.WriteLine("Location for Burlington...");
-        ILocation location = handlersProvider.GetLocationQueryHandler().GetLocationFor("Burlington", "US", "NC");
+        var location = handlersProvider.GetLocationQueryHandler().GetLocationFor("Burlington", "US", "NC");
         if (location == null)
         {
             Console.WriteLine("UNABLE TO GET LOCATION INFO");
@@ -45,5 +53,30 @@ class WeatherConsole
         
         Console.WriteLine("  Latitude: " + location.GetLatitude());
         Console.WriteLine("  Longitude: " + location.GetLongitude());
+    }
+
+    public static void HandleCurrentConditions()
+    {
+        Console.WriteLine("Conditions for Burlington...");
+        var conditions = handlersProvider.GetCurrentConditionsQueryHandler().GetCurrentConditionsFor(LATITUDE, LONGITUDE);
+        if (conditions == null)
+        {
+            Console.WriteLine("UNABLE TO GET CONDITIONS INFO");
+            return;
+        }
+        
+        Console.WriteLine("  Condition: " + conditions.GetConditionCode());
+        Console.WriteLine("  Description: " + conditions.GetConditionDescription());
+        Console.WriteLine("  Cloud coverage %: " + conditions.GetCloudCoveragePercent());
+        Console.WriteLine("  Temp in C: " + conditions.GetTemperatureInCelsius());
+        Console.WriteLine("  Max Temp in C: " + conditions.GetMaximumTemperatureInCelsius());
+        Console.WriteLine("  Min Temp in C: " + conditions.GetMinimumTemperatureInCelsius());
+        Console.WriteLine("  Humidity %: " + conditions.GetHumidityInPercent());
+        Console.WriteLine("  Visibiity in ft: " + conditions.GetVisibilityInFeet());
+        Console.WriteLine("  Wind speed in kt: " + conditions.GetWindSpeedInKnots());
+        Console.WriteLine("  Wind direction: " + conditions.GetWindDirectionInDegrees());
+        Console.WriteLine("  Sunrise UTC: " + conditions.GetSunriseTime());
+        Console.WriteLine("  Sunset UTC: " + conditions.GetSunsetTime());
+      
     }
 }
